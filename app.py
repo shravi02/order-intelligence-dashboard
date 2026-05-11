c1f3181a1237191a7efaca86758e056c2a69dfb6
Shravi Sharma <shravisharma@Shravis-MacBook-Air.local>
2026-05-11 22:33:44 +0530

    Removed insight icons

--- app.py
+++ app.py
@@ -230,9 +230,11 @@ elif authentication_status:
 
         # Smart Insights
         st.subheader("Smart Insights")
-        st.markdown('''<div class="insight-box">\u26a0 COD orders show highest return probability</div>''', unsafe_allow_html=True)
-        st.markdown('''<div class="insight-box">\u26a0 Orders delayed beyond 7 days are most likely to return</div>''', unsafe_allow_html=True)
-        st.markdown('''<div class="insight-box">\u2705 UPI orders have lowest RTO percentage</div>''', unsafe_allow_html=True)
+       st.markdown('''<div class="insight-box">COD orders show highest return probability</div>''', unsafe_allow_html=True)
+
+st.markdown('''<div class="insight-box">Orders delayed beyond 7 days are most likely to return</div>''', unsafe_allow_html=True)
+
+st.markdown('''<div class="insight-box">UPI orders have lowest RTO percentage</div>''', unsafe_allow_html=True)
 
         st.markdown("---")
 
