82a4e5d7d2b8ded00a4db9fbe9bdfef56139a3fa
Shravi Sharma <shravisharma@Shravis-MacBook-Air.local>
2026-05-12 04:44:15 +0530

    Fixed ML prediction input dataframe

--- app.py
+++ app.py
@@ -419,13 +419,19 @@ elif authentication_status:
         attempts_input    = st.slider("Delivery Attempts", 1,    5,   2)
         order_value_input = st.slider("Order Value",     200, 5000, 1500)
 
-        if st.button("Predict Return Risk"):
-            input_data = X.iloc[0:1].copy()
-            input_data[:] = 0
-            input_data["delay"]       = delay_input
-            input_data["attempts"]    = attempts_input
-            input_data["order_value"] = order_value_input
-            result = rf_model.predict(input_data)[0]
+	if st.button("Predict Return Risk"):
+
+	    input_data = pd.DataFrame(
+	        0,
+	        index=[0],
+	        columns=X.columns
+	    )
+
+	    input_data["delay"] = delay_input
+	    input_data["attempts"] = attempts_input
+	    input_data["order_value"] = order_value_input
+
+	    result = rf_model.predict(input_data)[0]
             if result == 1:
                 st.error("High Return Risk Predicted")
                 st.warning("Recommended Action: Call Customer")
